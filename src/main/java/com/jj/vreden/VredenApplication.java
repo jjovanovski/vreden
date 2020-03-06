eb0fb570d9fdefbcc572f7d0b2b5cef678fc7ba3
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2020-03-06 13:56:21 +0100

    Created project

---
+++ src/main/java/com/jj/vreden/VredenApplication.java
@@ -0,0 +1,13 @@
+package com.jj.vreden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VredenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VredenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jj/vreden/VredenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jj.vreden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VredenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
